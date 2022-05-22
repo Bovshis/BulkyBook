c0ca021a9e958f30e5513589536b380bf0317d80
bovshis <bovhisdaniil@gmail.com>
2022-05-22 23:05:04 +0300

    Add project files.

---
+++ AspNetFirstApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetFirstApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
