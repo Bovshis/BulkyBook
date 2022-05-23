f401dfa8a8e4513fd71275cd271211cbbd7f495e
bovshis <bovhisdaniil@gmail.com>
2022-05-23 22:00:53 +0300

    Add bootwatch theme

---
+++ AspNetFirstApp/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetFirstApp.Data;
+using AspNetFirstApp.Models;
+
+namespace AspNetFirstApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
+        }
+    }
+}
